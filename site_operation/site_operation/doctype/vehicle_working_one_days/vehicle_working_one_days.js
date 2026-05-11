18b59286730125ae59bb9ac688f8f32cc0a0bccd
Sukeshanee <zende.sukeshanee@kgce.edu.in>
2026-05-11 10:36:34 +0000

    Initial commit for vehicle working app

---
+++ site_operation/site_operation/doctype/vehicle_working_one_days/vehicle_working_one_days.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Sukeshanee and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("VEHICLE WORKING ONE DAYS", {
+// 	refresh(frm) {
+
+// 	},
+// });
