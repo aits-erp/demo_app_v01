9a3c7bd3659ee808185430b30b6f229554597d2f
Sukeshanee <zende.sukeshanee@kgce.edu.in>
2026-05-11 11:07:29 +0000

    added new Doctype

---
+++ site_operation/site_operation/doctype/machine_working_on_day/test_machine_working_on_day.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, aits and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMACHINEWORKINGONDAY(FrappeTestCase):
+	pass
